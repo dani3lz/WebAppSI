82a1f863782442b9ff448b6fb1b810e10fe074eb
Vitalie <ceban.vitalie@isa.utm.md>
2022-12-03 21:01:51 +0200

    added RSA

--- src/main/java/com/si/lab4/service/CryptServiceImpl.java
+++ src/main/java/com/si/lab4/service/CryptServiceImpl.java
@@ -13,7 +13,14 @@ import javax.crypto.SecretKey;
 import javax.crypto.spec.GCMParameterSpec;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
+import java.security.KeyFactory;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
 import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.PKCS8EncodedKeySpec;
 import java.util.Base64;
 
 @Service
@@ -36,19 +43,25 @@ public class CryptServiceImpl implements CryptService {
 
     private SecretKey keyAES = null;
     private SecretKey keyDES = null;
+    private PrivateKey keyRSAprivate = null;
+    private PublicKey keyRSApublic = null;
     private static final int KEY_SIZE_AES = 128;
     private static final int KEY_SIZE_DES = 56;
+    private static final int KEY_SIZE_RSA = 2048;
 
     private final Cipher encryptionCipherAES = Cipher.getInstance("AES/GCM/NoPadding");
     private final Cipher encryptionCipherDES = Cipher.getInstance("DES/CBC/PKCS5Padding");
+    private final Cipher encryptionCipherRSA = Cipher.getInstance("RSA");
     private final Cipher decryptionCipherDES = Cipher.getInstance("DES/CBC/PKCS5Padding");
     private final Cipher decryptionCipherAES = Cipher.getInstance("AES/GCM/NoPadding");
+    private final Cipher decryptionCipherRSA = Cipher.getInstance("RSA");
 
     @Override
     public TextResponse doOperation(TextRequest request) throws Exception {
         TextResponse response = new TextResponse();
         initAES();
         initDES();
+        initRSA();
         log.info("Starting to do something...");
         if (request.getMode().equals(Mode.ENCRYPT.toString())) {
             response = encryptText(request);
@@ -59,21 +72,22 @@ public class CryptServiceImpl implements CryptService {
         return response;
     }
 
-    private TextResponse decryptText(TextRequest request) {
-        if ("AES".equals(request.getAlgorithm())) {
+    private TextResponse decryptText(TextRequest request) throws NoSuchAlgorithmException, InvalidKeySpecException {
+        if (Algorithm.AES.toString().equals(request.getAlgorithm())) {
             byte[] decodedKey = Base64.getDecoder().decode(request.getKey());
             SecretKey originalKey = new SecretKeySpec(decodedKey, 0, decodedKey.length, Algorithm.AES.toString());
             return aesDecrypt(request.getInputText(), originalKey);
-        } else if ("DES".equals(request.getAlgorithm())) {
-
+        } else if (Algorithm.DES.toString().equals(request.getAlgorithm())) {
             byte[] decodedKey = Base64.getDecoder().decode(request.getKey());
             SecretKey originalKey = new SecretKeySpec(decodedKey, 0, decodedKey.length, Algorithm.DES.toString());
             return desDecrypt(request.getInputText(), originalKey);
-        } else if ("RSA".equals(request.getAlgorithm())) {
-            // TODO
+        } else if (Algorithm.RSA.toString().equals(request.getAlgorithm())) {
+            byte[] decodedKey = Base64.getDecoder().decode(request.getKey());
+            KeyFactory kf = KeyFactory.getInstance("RSA");
+            PrivateKey originalKey = kf.generatePrivate(new PKCS8EncodedKeySpec(decodedKey));
+            return rsaDecrypt(request.getInputText(), originalKey);
         } else
-            return null;
-        throw new RuntimeException("mda");
+            throw new SomethingIsWrongException();
     }
 
     private TextResponse encryptText(TextRequest request) {
@@ -83,10 +97,9 @@ public class CryptServiceImpl implements CryptService {
         } else if (Algorithm.DES.toString().equals(request.getAlgorithm())) {
             return desEncrypt(request.getInputText());
         } else if (Algorithm.RSA.toString().equals(request.getAlgorithm())) {
-            // TODO
+            return rsaEncrypt(request.getInputText());
         } else
-            return null;
-        throw new RuntimeException("mda");
+            throw new SomethingIsWrongException();
     }
 
     private TextResponse aesEncrypt(String inputText) {
@@ -147,7 +160,6 @@ public class CryptServiceImpl implements CryptService {
     public String encryptDES(String data, SecretKey key) {
         try {
             byte[] dataInBytes = data.getBytes();
-            // encryptionCipherDES = Cipher.getInstance("DES/CBC/PKCS5Padding");
             encryptionCipherDES.init(Cipher.ENCRYPT_MODE, key);
             byte[] encryptedBytes = encryptionCipherDES.doFinal(dataInBytes);
             return encode(encryptedBytes);
@@ -177,6 +189,49 @@ public class CryptServiceImpl implements CryptService {
         }
     }
 
+    private TextResponse rsaEncrypt(String inputText) {
+        try {
+            TextResponse response = new TextResponse();
+            response.setOutputText(encryptRSA(inputText, keyRSApublic));
+            response.setKey(Base64.getEncoder().encodeToString(keyRSAprivate.getEncoded()));
+            return response;
+        } catch (Exception e) {
+            throw new SomethingIsWrongException();
+        }
+    }
+
+    public String encryptRSA(String data, PublicKey key) {
+        try {
+            byte[] dataInBytes = data.getBytes();
+            encryptionCipherRSA.init(Cipher.ENCRYPT_MODE, key);
+            byte[] encryptedBytes = encryptionCipherRSA.doFinal(dataInBytes);
+            return encode(encryptedBytes);
+        } catch (Exception e) {
+            throw new SomethingIsWrongException();
+        }
+    }
+
+    private TextResponse rsaDecrypt(String inputText, PrivateKey key) {
+        try {
+            TextResponse response = new TextResponse();
+            response.setOutputText(decryptRSA(inputText, key));
+            return response;
+        } catch (Exception e) {
+            throw new SomethingIsWrongException();
+        }
+    }
+
+    public String decryptRSA(String encryptedData, PrivateKey key) {
+        try {
+            byte[] dataInBytes = decode(encryptedData);
+            decryptionCipherRSA.init(Cipher.DECRYPT_MODE, key);
+            byte[] decryptedBytes = decryptionCipherRSA.doFinal(dataInBytes);
+            return new String(decryptedBytes);
+        } catch (Exception e) {
+            throw new SomethingIsWrongException();
+        }
+    }
+
     private String encode(byte[] data) {
         return Base64.getEncoder().encodeToString(data);
     }
@@ -185,16 +240,36 @@ public class CryptServiceImpl implements CryptService {
         return Base64.getDecoder().decode(data);
     }
 
-    public void initAES() throws Exception {
-        KeyGenerator keyGenerator = KeyGenerator.getInstance(Algorithm.AES.toString());
-        keyGenerator.init(KEY_SIZE_AES);
-        this.keyAES = keyGenerator.generateKey();
+    public void initAES() {
+        try {
+            KeyGenerator keyGenerator = KeyGenerator.getInstance(Algorithm.AES.toString());
+            keyGenerator.init(KEY_SIZE_AES);
+            this.keyAES = keyGenerator.generateKey();
+        } catch (Exception e) {
+            throw new SomethingIsWrongException();
+        }
     }
 
-    public void initDES() throws Exception {
-        KeyGenerator keyGenerator = KeyGenerator.getInstance(Algorithm.DES.toString());
-        keyGenerator.init(KEY_SIZE_DES);
-        this.keyDES = keyGenerator.generateKey();
+    public void initDES() {
+        try {
+            KeyGenerator keyGenerator = KeyGenerator.getInstance(Algorithm.DES.toString());
+            keyGenerator.init(KEY_SIZE_DES);
+            this.keyDES = keyGenerator.generateKey();
+        } catch (Exception e) {
+            throw new SomethingIsWrongException();
+        }
+    }
+
+    public void initRSA() {
+        try {
+            KeyPairGenerator generator = KeyPairGenerator.getInstance("RSA");
+            generator.initialize(KEY_SIZE_RSA);
+            KeyPair pair = generator.generateKeyPair();
+            this.keyRSAprivate = pair.getPrivate();
+            this.keyRSApublic = pair.getPublic();
+        } catch (Exception e) {
+            throw new SomethingIsWrongException();
+        }
     }
 
 
